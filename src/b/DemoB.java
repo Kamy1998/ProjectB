9b6fd63611e7330c76089681b60c30410cc55433
Kamy <Kamy@4Ever.mynetworksettings.com>
2024-01-07 13:28:50 -0500

    Add getCity()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -1,17 +1,21 @@
 package b;
 
 public class DemoB {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		System.out.println("Tuesday");
-		
+
 		System.out.println("ABC");
-		
+
 	}
 
-public static void add( int a, int b){
-	System.out.println(a + b);
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 
-}
+	}
+
+	public static void getCity(String city) {
+		System.out.println(city);
+	}
 }
